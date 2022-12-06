f5e1a11ae0e236f03bf0b5f5bbb4e29dd72c9710
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2022-12-06 22:43:11 +0100

    Blocking and Non-Blocking Code + Node.js - Looking Behind the Scenes

--- app.js
+++ app.js
@@ -19,14 +19,15 @@ const server = http.createServer((req, res) => {
       console.log(chunk);
       body.push(chunk);
     });
-    req.on('end', () => {
+    return req.on('end', () => {
       const parsedBody = Buffer.concat(body).toString();
       const message = parsedBody.split('=')[1];
-      fs.writeFileSync('message.txt', message);
+      fs.writeFile('message.txt', message, err => {
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
+      });
     });
-    res.statusCode = 302;
-    res.setHeader('Location', '/');
-    return res.end();
   }
   res.setHeader('Content-Type', 'text/html');
   res.write('<html>');
