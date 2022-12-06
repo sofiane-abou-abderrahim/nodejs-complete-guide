d9bc338164f9ea4c03806d4a3e4e5e31975e78a9
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2022-12-06 16:38:17 +0100

    node lifecycle & event loop + controlling node.js process + requests + sending reponses

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const http = require('http');
 
 const server = http.createServer(function (req, res) {
-  console.log(req);
+  console.log(req.url, req.method, req.headers);
+  // process.exit();
+  res.setHeader('Content-Type', 'text/html');
+  res.write('<html>');
+  res.write('<head><title>My First Page</title></head>');
+  res.write('<body><h1>Hello from my Node.js server!</h1></body>');
+  res.write('</html>');
+  res.end();
 });
 
 server.listen(3000);
