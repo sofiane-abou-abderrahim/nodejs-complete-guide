62e034ae4f1faccf54ae57275ebb9757471b0e1a
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2022-12-06 16:56:08 +0100

    routing requests + redirecting requests

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
 const http = require('http');
+const fs = require('fs');
 
-const server = http.createServer(function (req, res) {
-  console.log(req.url, req.method, req.headers);
-  // process.exit();
+const server = http.createServer((req, res) => {
+  const url = req.url;
+  const method = req.method;
+  if (url === '/') {
+    res.write('<html>');
+    res.write('<head><title>Enter Message</title><head>');
+    res.write(
+      '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>'
+    );
+    res.write('</html>');
+    return res.end();
+  }
+  if (url === '/message' && method === 'POST') {
+    fs.writeFileSync('message.txt', 'DUMMY');
+    res.statusCode = 302;
+    res.setHeader('Location', '/');
+    return res.end();
+  }
   res.setHeader('Content-Type', 'text/html');
   res.write('<html>');
-  res.write('<head><title>My First Page</title></head>');
-  res.write('<body><h1>Hello from my Node.js server!</h1></body>');
+  res.write('<head><title>My First Page</title><head>');
+  res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
   res.write('</html>');
   res.end();
 });
