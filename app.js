b9b73aee4bb7711cd20446eded772af5958a15ba
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2022-12-08 17:22:55 +0100

    installing express.js + adding middleware

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 const http = require('http');
 
-const routes = require('./routes');
+const express = require('express');
 
-console.log(routes.someText);
+const app = express();
 
-const server = http.createServer(routes.handler);
+app.use((req, res, next) => {
+    console.log('In the middleware!');
+    next(); // Allows the request to continue to the next middleware in line
+});
+
+app.use((req, res, next) => {
+    console.log('In another middleware!');
+    res.send('<h1>Hello from Express!</h1>');
+});
+
+const server = http.createServer(app);
 
 server.listen(3000);
