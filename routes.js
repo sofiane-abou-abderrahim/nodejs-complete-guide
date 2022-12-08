f44994f91115413310fe236861353bf3cfa81565
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2022-12-08 14:59:58 +0100

    nodemon autorestarts + global & local npm + different error types + debugger

--- routes.js
+++ routes.js
@@ -20,7 +20,7 @@ const requestHandler = (req, res) => {
     });
     return req.on('end', () => {
       const parsedBody = Buffer.concat(body).toString();
-      const message = parsedBody.split('=')[1];
+      const message = parsedBody.split('=')[0];
       fs.writeFile('message.txt', message, err => {
         res.statusCode = 302;
         res.setHeader('Location', '/');
@@ -28,6 +28,7 @@ const requestHandler = (req, res) => {
       });
     });
   }
+
   res.setHeader('Content-Type', 'text/html');
   res.write('<html>');
   res.write('<head><title>My First Page</title><head>');
