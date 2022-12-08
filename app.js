8af1d0ccdebf822282d1072940f267bd36d67a00
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2022-12-08 18:19:59 +0100

    parsing incoming requests + limiting middleware execution to post requests

--- app.js
+++ app.js
@@ -1,26 +1,21 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-// app.use((req, res, next) => {
-//   console.log('First Middleware');
-//   next();
-// });
+app.use(bodyParser.urlencoded({extended: false}));
 
-// app.use((req, res, next) => {
-//   console.log('Second Middleware');
-//   res.send('<p>Assignment solved (almost!)</p>');
-// });
+app.use('/add-product', (req, res, next) => {
+  res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
+});
 
-app.use('/users', (req, res, next) => {
-    console.log('/users middleware');
-    res.send('<p>The Middleware that handles just /users</p>');
+app.post('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
 });
 
 app.use('/', (req, res, next) => {
-    console.log('/ middleware');
-    res.send('<p>The Middleware that handles just /</p>');
+  res.send('<h1>Hello from Express!</h1>');
 });
-
 
 app.listen(3000);
